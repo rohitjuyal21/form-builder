58d45bc02803dc0442280236f2265a7f94bbabbd
rohitjuyal21 <rohitjuyal2003@gmail.com>
2024-12-09 09:53:13 +0530

    Update form page

--- app/form/[formId]/page.tsx
+++ app/form/[formId]/page.tsx
@@ -1,6 +1,10 @@
 import SharedForm from "@/components/SharedForm/SharedForm";
 
-export default async function page({ params }: { params: { formId: string } }) {
+export default async function page({
+  params,
+}: {
+  params: Promise<{ formId: string }>;
+}) {
   const { formId } = await params;
   return <SharedForm formId={formId} />;
 }
